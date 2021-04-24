666ecd70970bada265afff28a07ba2e3cf8629ca
faran4engg <faran4engg@gmail.com>
2021-04-24 21:32:19 +0530

    update animation

--- src/app/domains/Common/components/sidepanel/SidePanel.tsx
+++ src/app/domains/Common/components/sidepanel/SidePanel.tsx
@@ -9,13 +9,13 @@ const SidePanel = ({ children, isSidePanelOpen, handleIsSidePanelOpen }) => {
         handleIsSidePanelOpen={handleIsSidePanelOpen}
       />
       <div
-        className={`fixed right-0 w-80 overflow-y-auto transition duration-300 ease-out transform z-30 h-full shadow-left shadow-md bg-light-mode dark:bg-dark-mode ${
+        className={`fixed right-0 w-full md:w-80 overflow-y-auto transition duration-500 ease-out transform z-30 h-full shadow-left shadow-md bg-light-mode dark:bg-dark-mode ${
           isSidePanelOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
         <div
           onClick={() => handleIsSidePanelOpen(!isSidePanelOpen)}
-          className="z-50 flex flex-col items-end p-2 cursor-pointer "
+          className="z-50 flex flex-col items-end p-2 cursor-pointer"
         >
           <SvgIcon
             className="w-8 h-8 bg-light-mode dark:bg-dark-mode"
